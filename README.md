aad8f53fb8e680ac15ea27c1c1545d2e5584b7c3
datalater <the7mincheol@gmail.com>
2017-12-08 15:13:28 +0900

    README.md

---
+++ README.md
@@ -0,0 +1,11 @@
+## How to modify page per number
+
+1. 셀레늄 드라이버가 실행된 상태에서 크롬 개발자도구를 열고 Element 탭에서 pagePerNumber_false를 찾아서 직접 숫자를 입력한다.
+
+![PagePerNumber-1.png](images/PagePerNumber-1.png)
+
+2. 드랍다운 버튼에서 변경된 옵션을 클릭한다.
+
+![PagePerNumber-1.png](images/PagePerNumber-2.png)
+
+---
--- crawler.py
+++ crawler.py
@@ -1,3 +1,5 @@
+### Contributed by 손규빈, 최완재, 오학
+
 import time
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
@@ -9,34 +11,38 @@ import getpass
 from bs4 import BeautifulSoup
 import os, re
 
-###########################
-#                         #
-#   사람이 직접 하는 단계   #
-#                         #
-###########################
+######################
+#                    #
+#   Run one by one   #
+#                    #
+######################
 
-# 1. 셀레늄으로 크롬 브라우저를 연다.
+# 셀레늄으로 크롬 브라우저를 연다.
 driver = webdriver.Chrome()
 time.sleep(2)
 
-# 2. '복지' 탭에 접속한다.
+# '복지' 탭에 접속한다.
 driver.get('https://www.open.go.kr/search/theme/theme.do?themecd=00028')
 
-# 3. 현재 웹 페이지의 내용을 긁어온다.
+# 현재 웹 페이지의 내용을 긁어온다.
 soup = BeautifulSoup(driver.page_source, 'lxml')
 
-# 4. 전체 문서를 한 페이지에 수동으로 로딩한다.
-# e.g. 크롬 개발자도구를 열고 20개씩 보기를 1500개씩 보기로 수동으로 바꾼다.
+#########################################
+#                                       #
+#   Modify page per number in browser   #
+#                                       #
+#########################################
 
+# 전체 문서를 한 페이지에 수동으로 로딩한다.
+# README.md 참조
 
-###############################
-#                             #
-#   셀레늄으로 자동 작업 단계   #
-#                             #
-###############################
+#####################
+#                   #
+#   Run all below   #
+#                   #
+#####################
 
-# 원문 파일 목록에서 필요한 함수 인자를 가져온다.
-
+# 원문 목록에서 파일 다운로드에 필요한 함수 인자를 가져온다.
 prdnDt_list = []
 prdnNstRgstNo_list= []
 for tr in soup.select("#result_false tr"):
@@ -45,11 +51,10 @@ for tr in soup.select("#result_false tr"):
     prdnDt_list.append(tmp[0])
     prdnNstRgstNo_list.append(tmp[1])
 
-
 for prdnDt, prdnNstRgstNo in zip(prdnDt_list, prdnNstRgstNo_list):
-    start_time = time.time()
+    # start_time = time.time()
 
-    # 현재 웹브라우저에서 새 탭을 열고 이동한다.
+    # 현재 브라우저에서 새 탭을 열고 이동한다.
     driver.execute_script("window.open('')")
     driver.switch_to_window(driver.window_handles[1])
 
@@ -62,16 +67,17 @@ for prdnDt, prdnNstRgstNo in zip(prdnDt_list, prdnNstRgstNo_list):
 
     # 파일을 다운로드 할 때 필요한 javascript 인자를 가져와서 함수를 실행한다.
     download_script = file_soup.select("p.fr")[0].a['onclick']
-
     driver.execute_script(download_script)
 
-    time.sleep(10)
+    # 파일 다운로드에 필요한 시간을 10초로 설정한다.
+    delay = 10
+    time.sleep(delay)
     try:
         driver.switch_to_alert().accept();
     except:
-        print("alert 없음")
+        print("[%d]초 내 다운로드 성공 : [%s]" % (delay, file_name))
 
     # 다운로드가 완료되었으니 새 탭을 닫고, 목록이 있는 탭으로 이동한다.
     driver.execute_script("window.close()")
     driver.switch_to_window(driver.window_handles[0])
-    print("Elapsed time: %s seconds" %(time.time() - start_time))
+    # print("\tElapsed time: %s seconds" %(time.time() - start_time))
